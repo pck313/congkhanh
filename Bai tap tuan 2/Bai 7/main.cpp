745e2943b6e72b6921aef2ed4b21794ab3eb87a0
pck313 <23021596@vnu.edu.vn>
2024-03-03 16:23:24 +0700

    Bài 7 *

---
+++ Bai tap tuan 2/Bai 7/main.cpp
@@ -0,0 +1,25 @@
+//Viết chương trình DistinctNumbers với nhiệm vụ đọc một chuỗi số từ input chuẩn (bàn phím) kết thúc ở số âm đầu tiên, và in ra các số đó theo đúng thứ tự, nhưng nếu có các số liền nhau cùng giá trị thì chỉ in ra một lần. Chương trình chỉ được dùng các biến kiểu cơ bản, không được dùng mảng, string, hay các cấu trúc dữ liệu khác.
+//Ví dụ input:
+//1 2 2 2 2 4 2 3 5 5 -1
+//Output tương ứng:
+//1 2 4 2 3 5 -1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n;
+    cout << n << " ";
+    while (m > 0)
+    {
+        cin >> m;
+        if (m != n)
+        {
+            cout << m <<" ";
+        }
+        n = m;
+    }
+}
--- Bai tap tuan 2/bai 6/main.cpp
+++ Bai tap tuan 2/bai 6/main.cpp
@@ -12,6 +12,6 @@ int main()
     {
         for(int k=i;k<=n;k++) cout<<k<<" ";
         for(int k=1;k<=i-1;k++) cout<<k<<" ";
-        cout<<endl;
+        cout << endl;
     }
 }
